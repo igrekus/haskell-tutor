36fe9c911ee016e9a71c62cd27392cee81f50187
igrekus <upload234@mail.ru>
2020-08-26 19:59:30 +0300

    WIP: add let in listcomp example

--- funcs.hs
+++ funcs.hs
@@ -116,3 +116,6 @@ cylArea r h =
     let sideArea = 2 * pi * r * h
         topArea = pi * r ^ 2
     in  sideArea + 2 * topArea
+
+calcBmis' :: (RealFloat a) => [(a, a)] -> [a]
+calcBmis' xs = [bmi | (w, h) <- xs, let bmi = w / h ^ 2]
