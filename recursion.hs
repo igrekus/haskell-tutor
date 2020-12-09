0c25ebc99693c8a6530067b9d3fab5ba41a8f28a
igrekus <upload234@mail.ru>
2020-12-09 17:17:30 +0300

    MISC: add recursive max without where clause

--- recursion.hs
+++ recursion.hs
@@ -5,3 +5,10 @@ maximum' (x:xs)
             | x > maxTail = x
             | otherwise = maxTail
             where maxTail = maximum' xs
+
+maximum'' :: (Ord a) => [a] -> a
+maximum'' [] = error "empty list"
+maximum'' [x] = x
+maximum'' (x:xs)
+    | x > maximum'' xs = x
+    | otherwise = maximum'' xs
