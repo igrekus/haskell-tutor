3e789c319964d9f1cba48f8cf551518296ca1826
igrekus <upload234@mail.ru>
2020-12-10 15:42:28 +0300

    MISC: add quicksort with let clause

--- recursion.hs
+++ recursion.hs
@@ -17,3 +17,10 @@ maximum''' :: (Ord a) => [a] -> a
 maximum''' [] = error "empty list"
 maximum''' [x] = x
 maximum''' (x:xs) = max x (maximum''' xs)
+
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (x:xs) =
+    let smallerSorted = quicksort [a | a <- xs, a <= x]
+        biggerSorted = quicksort [a | a <- xs, a > x]
+    in smallerSorted ++ [x] ++ biggerSorted
