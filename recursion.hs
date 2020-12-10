03e24df9a75890a0807bdf9a210a7818559a15fd
igrekus <upload234@mail.ru>
2020-12-10 15:54:04 +0300

    MISC: add quicksort with filter

--- recursion.hs
+++ recursion.hs
@@ -36,3 +36,8 @@ quicksort'' :: (Ord a) => [a] -> [a]
 quicksort'' [] = []
 quicksort'' (x:xs) =
     quicksort'' [a | a <- xs, a <= x] ++ [x] ++ quicksort'' [a | a <- xs, a > x]
+
+quicksort''' :: (Ord a) => [a] -> [a]
+quicksort''' [] = []
+quicksort''' (x:xs) = 
+    quicksort''' (filter (<= x) xs) ++ [x] ++ quicksort''' (filter (> x) xs)
