e8a9f3e31ba8eb16456958c434ad1a8493d8a8e8
igrekus <upload234@mail.ru>
2020-12-28 15:55:07 +0300

    WIP: add collatz sequence generator

--- hof.hs
+++ hof.hs
@@ -46,3 +46,9 @@ filter' _ [] = []
 filter' p (x:xs)
     | p x = x : filter' p xs
     | otherwise = filter' p xs
+
+collatz :: (Integral a) => a -> [a]
+collatz 1 = [1]
+collatz n
+    | even n = n: collatz (n `div` 2)
+    | odd n = n: collatz (3 * n + 1)
