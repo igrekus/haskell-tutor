1274de7a05d76f671714ab6f4c08a1c2311f77a7
igrekus <upload234@mail.ru>
2021-02-24 18:01:39 +0300

    WIP: add some Data module exaples

---
+++ modules.hs
@@ -0,0 +1,23 @@
+import Data.List
+import Data.Char
+
+numUniques :: (Eq a) => [a] -> Int
+numUniques xs = length (nub xs)
+
+numUniques' :: (Eq c) => [c] -> Int
+numUniques' = length . nub
+
+search :: (Eq a) => [a] -> [a] -> Bool
+search needle haysack = 
+    let nlen = length needle
+    in foldl (\acc x -> (take nlen x == needle) || acc) False (tails haysack)
+
+encode :: Int -> String -> String 
+encode shift message = 
+    let ords = map ord message
+        shifted = map (+ shift) ords
+    in map chr shifted
+
+encode' :: Int -> String -> String 
+encode' shift = 
+    map (chr . (+ shift) . ord)
